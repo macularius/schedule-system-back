8cdfbe77074e7b8752dd613bfeb1c14da5bc74ca
Igor Kovatsenko <igor.kovatsenko@yandex.ru>
2019-06-07 17:06:53 +0400

    идентификация из базы

--- app/controllers/Authenticate.go
+++ app/controllers/Authenticate.go
@@ -2,8 +2,9 @@ package controllers
 
 import (
 	"crypto/md5"
+	"database/sql"
 	"fmt"
-	"io"
+	"myapp/app"
 	"strings"
 
 	"github.com/revel/revel"
@@ -18,55 +19,42 @@ type Authenticate struct {
 func (c *Authenticate) Login() revel.Result {
 	fmt.Print("\nAuthorization\n" + c.Request.GetHttpHeader("Authorization") + "\n\n")
 
-	if c.Request.GetHttpHeader("Authorization") == "" {
-		c.Response.Status = 401
-		str, _ := GetDigestString("users@schedules", c.ClientIP)
-		fmt.Print("\nGetDigestString\n" + str + "\n\n")
-		digestString, err := GetDigestString("users@schedules", c.ClientIP)
+	if c.Request.GetHttpHeader("Authorization") != "" {
+		username, realmVal, nonceVal, digestURIVal, responseVal := getDigestHeaders(c.Request.GetHttpHeader("Authorization"))
+		method := c.Request.Method
+		password := ""
+
+		db, err := sql.Open("postgres", app.GetConnectionString())
 		if err != nil {
-			return c.Render(Failed(err))
+			return c.RenderJSON(Failed(err))
 		}
-		c.Response.Out.Header().Add("WWW-Authenticate", digestString)
+		defer db.Close()
 
-		return c.Render()
+		row := db.QueryRow(sqlGetUserString(username))
+		if err = row.Scan(&password); err != nil {
+			return c.RenderJSON(Failed(err))
+		}
+
+		ha1 := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s:%s", username, realmVal, password))))
+		ha2 := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s", method, digestURIVal))))
+
+		serverResp := fmt.Sprintf("%x", md5.Sum([]byte(strings.Join([]string{ha1, nonceVal, ha2}, ":"))))
+
+		if serverResp == responseVal {
+			fmt.Print("\nAll right\n", serverResp, "\n", responseVal, "\n\n")
+
+			return c.RenderJSON(Succes("All right " + "server: " + serverResp + "   val: " + responseVal))
+		}
 	}
 
-	username, realmVal, nonceVal, digestURIVal, responseVal := getDigestHeaders(c.Request.GetHttpHeader("Authorization"))
-	method := c.Request.Method
-	password := "ikov"
-	nonceVal = fmt.Sprintf("%s", nonceVal)
-
-	// fmt.Print("\n\n", "username=", username, "\nrealm=", realmVal, "\nnonce=", nonceVal, "\nuriVal=", digestURIVal, "\nuri=", c.Request.GetPath(), "\nmethod=", method, "\n\n")
-
-	ha1 := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s:%s", username, realmVal, password))))
-	ha2 := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s", method, digestURIVal))))
-
-	serverResp := fmt.Sprintf("%x", md5.Sum([]byte(strings.Join([]string{ha1, nonceVal, ha2}, ":"))))
-
-	// fmt.Print("\n\n", "ha1=", ha1, "\nha2=", ha2, "\nresp=", serverResp, "\n\n")
-
-	if serverResp == responseVal {
-
-		fmt.Print("\nAll right\n", serverResp, "\n", responseVal, "\n\n")
-	} else {
-
-		fmt.Print("\nAll bad\n", serverResp, "\n", responseVal, "\n\n")
+	c.Response.Status = 401
+	digestString, err := GetDigestString("users@schedules", c.ClientIP)
+	if err != nil {
+		return c.RenderJSON(Failed(err))
 	}
+	c.Response.Out.Header().Add("WWW-Authenticate", digestString)
 
-	return c.RenderJSON(Succes(c.Request.Header.Get("Authorization")))
-}
-
-func h(data string) []byte {
-	h := md5.New()
-	io.WriteString(h, data)
-
-	return h.Sum(nil)
-}
-func kd(secret string, data string) []byte {
-	h := md5.New()
-	io.WriteString(h, fmt.Sprintf("%s:%s", secret, data))
-
-	return h.Sum(nil)
+	return c.Render()
 }
 
 func getDigestHeaders(response string) (string, string, string, string, string) {
@@ -88,4 +76,8 @@ func getDigestHeaders(response string) (string, string, string, string, string) 
 	responseVal := respKeysVals["response"]
 
 	return username, realm, nonce, uri, responseVal
+}
+
+func sqlGetUserString(username string) string {
+	return fmt.Sprintf("SELECT password FROM users WHERE login='%s'", username)
 }
